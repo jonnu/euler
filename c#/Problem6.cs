f579fdf2479a2cbeb22aeb0f6d7699f14ee25125
Jon <jontce@gmail.com>
2013-04-14 18:28:24 +0300

    adding problem 6

---
+++ c#/Problem6.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Problem6 : Problem
+    {
+        public override void process()
+        {
+            IEnumerable<Int32> numbers = Enumerable.Range(1, 100);
+            Double sumSquareDifference = Math.Pow(numbers.Sum(), 2) - numbers.Select(i => Math.Pow(i, 2)).Sum();
+
+            System.Console.WriteLine(sumSquareDifference);
+            System.Console.ReadKey();
+        }
+
+    }
+}
