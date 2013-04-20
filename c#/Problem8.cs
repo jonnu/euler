a9ce0b70617d9667f52a62e64936654d3fe35123
Jon <jontce@gmail.com>
2013-04-20 10:33:03 +0300

    Adding shell of Problem 8

---
+++ c#/Problem8.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Problem8 : Problem
+    {
+        public override void process()
+        {
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
