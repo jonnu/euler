e79468c2165060c7cb061a38b4c8b0e636ee6bdd
Jon Ellis-Jones <john.ellis@skype.net>
2013-04-20 07:47:41 +0100

    Adding old PHP implementation(s)

---
+++ php/7.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Return the n-th prime
+ *
+ * @param integer $n Prime to find
+ *
+ * @return integer
+ */
+function getNthPrime ($n) {
+
+    $i      = 2;
+    $primes = array($i);
+
+    for (; count($primes) !== $n; ++$i) {
+
+        $composite = false;
+        $sqrt      = sqrt($i);
+
+        foreach ($primes as $prime) {
+
+            if ($prime > $sqrt) {
+                break;
+            }
+
+            if ($i % $prime === 0) {
+                $composite = true;
+                break;
+            }
+        }
+
+        if (!$composite) {
+            $primes[] = $i;
+        }
+    }
+
+    return $primes[$n - 1];
+}
+
+
+echo sprintf("10001th prime: %s\n", getNthPrime(10001));
