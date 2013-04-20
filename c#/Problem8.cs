9dd304ff910fe88a071906a68ef01caad9da297f
Jon <jontce@gmail.com>
2013-04-20 19:52:46 +0300

    Completed problem 8

--- c#/Problem8.cs
+++ c#/Problem8.cs
@@ -10,8 +10,29 @@ namespace Euler
     {
         public override void process()
         {
-            Console.WriteLine("");
+            Int32 chunk = 5, max = 0;
+            String data = readTextFile("Problem8.txt");
+
+            for (Int32 p = 0; p < data.Length - chunk; p++)
+            {
+                Int32 product = productOfAllDigits(data.Substring(p, chunk));
+                if (product > max) {
+                    max = product;
+                }
+            }
+
+            Console.WriteLine(max);
             Console.ReadKey();
+        }
+
+        private Int32 productOfAllDigits(String input)
+        {
+            if (input.Contains("0") || input.Length == 0)
+                return 0;
+
+            return input.ToCharArray()
+                .Select(x => Int32.Parse(x.ToString()))
+                .Aggregate((x, y) => x * y);
         }
     }
 }
