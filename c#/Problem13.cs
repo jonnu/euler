77ff331011e065bf418ef19cbc873d6a3e03878a
Jon <jontce@gmail.com>
2013-04-21 13:00:03 +0300

    Solution for problem 13 (somewhat cheesy? using BigInteger namespace)

---
+++ c#/Problem13.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Problem13 : Problem
+    {
+        public override void process()
+        {
+            List<BigInteger> values = readBigIntegersFromFile("Problem13.txt");
+
+            string firstTenDigits = values
+                .Aggregate((current, next) => current + next)
+                .ToString()
+                .Substring(0, 10);
+
+            Console.WriteLine(firstTenDigits);
+            Console.ReadKey();
+        }
+
+        private List<BigInteger> readBigIntegersFromFile(string filename)
+        {
+            string raw = readTextFile(filename, false);
+            List<BigInteger> data = new List<BigInteger>();
+
+            using (StringReader reader = new StringReader(raw))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    data.Add(BigInteger.Parse(line));
+                }
+            }
+
+            return data;
+        }
+    }
+}
