cf84b399934113c37cdff8291b51b044c8c3afcc
Jon <jontce@gmail.com>
2013-04-21 14:49:56 +0300

    System.Numerics.BigInteger makes this trivial (again)

---
+++ c#/Problem16.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Problem16 : Problem
+    {
+        public override void process()
+        {
+            BigInteger largePower = BigInteger.Pow(2, 1000);
+            Int64 sumAllTheDigits = largePower
+                .ToString()
+                .ToCharArray()
+                .Select(n => Int32.Parse(n.ToString()))
+                .Sum();
+
+            Console.WriteLine(sumAllTheDigits);
+            Console.ReadKey();
+        }
+    }
+}
