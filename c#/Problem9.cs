9cd9f550b5042cb6d2509ae5b39fa1af2deb2836
Jon <jontce@gmail.com>
2013-04-21 11:15:44 +0300

    Completed problem 9

---
+++ c#/Problem9.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Problem9 : Problem
+    {
+        public override void process()
+        {
+            Int64 product = findProductOfPythagoreanTriplet(1000);
+
+            Console.WriteLine(product);
+            Console.ReadKey();
+        }
+
+        private Int64 findProductOfPythagoreanTriplet(Int32 limit)
+        {
+            Int32 a, b, c;
+
+            for (a = 1; a < 500; a++)
+            {
+                for (b = a + 1; b < 500; b++)
+                {
+                    if (a >= b)
+                        continue;
+
+                    c = limit - b - a;
+                    if (b >= c)
+                        continue;
+
+                    if (a + b + c != limit)
+                        continue;
+
+                    if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+                        return a * b * c;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
