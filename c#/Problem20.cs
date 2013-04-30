86f5e68b6ceb32e115014e34e1ad66d37434675e
Jon Ellis-Jones <john.ellis@skype.net>
2013-04-30 14:05:00 +0100

    Solution for problem 20, leveraging the power of C#'s BigInteger type

---
+++ c#/Problem20.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler
+{
+    class Problem20 : Problem
+    {
+        public override void process()
+        {
+            Int32 factor = 100;
+            Int32 sumOfFactoral = Enumerable.Range(1, factor)
+                .Select(i => (BigInteger)i)
+                .Aggregate(BigInteger.Multiply)
+                .ToString()
+                .ToCharArray()
+                .Select(j => Int32.Parse(j.ToString()))
+                .Sum();
+
+            Console.WriteLine("Sum of digits in {0}! = {1}", factor, sumOfFactoral);
+            Console.ReadKey();
+        }
+    }
+}
