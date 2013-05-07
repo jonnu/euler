b2b77199210ac43744720190f02582a1c7801b16
jonnu <jontce@gmail.com>
2013-05-07 10:46:25 +0300

    Updates to problem 27
    Interesting to learn that caching the primes in a List<long> was a bad
    move. Also discovered that b MUST be prime for n=0, which vastly reduces
    the search space. Removing the cache and including this optimisation cut
    the processing time from 3.1s down to 0.007s.

--- c#/Problem27.cs
+++ c#/Problem27.cs
@@ -2,32 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
 namespace Euler
 {
     class Problem27 : Problem
     {
-        private Dictionary<string, int> coefficients = new Dictionary<string, int>() { { "a", 0 }, { "b", 0 } };
-        private List<long> primeCache = new List<long>();
+        private int coefficient_a = 0;
+        private int coefficient_b = 0;
+        private int maximumPrimes = 0;
 
         public override void process()
         {
-            int maxPrimes = 0;
-            for (int a = -999; Math.Abs(a) < 1000; a++)
+            for (int b = -999; Math.Abs(b) < 1000; b++)
             {
-                for (int b = -999; Math.Abs(b) < 1000; b++)
+                // b *must* be prime if n = 0
+                if (!IsPrime(b))
+                    continue;
+
+                for (int a = -999; Math.Abs(a) < 1000; a++)
                 {
                     int primes = GetConsecutivePrimes(a, b);
-                    if (primes > maxPrimes)
+                    if (primes > maximumPrimes)
                     {
-                        maxPrimes = primes;
-                        coefficients["a"] = a;
-                        coefficients["b"] = b;
+                        maximumPrimes = primes;
+                        coefficient_a = a;
+                        coefficient_b = b;
                     }
-                    
                 }
             }
-            Console.WriteLine("a = {0}, b = {1}, Product: {2}", coefficients["a"], coefficients["b"], coefficients["a"] * coefficients["b"]);
+
+            Console.WriteLine("a = {0}, b = {1}, Product: {2}", coefficient_a, coefficient_b, coefficient_a * coefficient_b);
         }
 
         private int GetConsecutivePrimes(int a, int b)
@@ -51,16 +54,12 @@ namespace Euler
             if (n <= 1)
                 return false;
 
-            if (primeCache.Contains(n))
-                return true;
-
             for (int i = 2; Math.Pow(i, 2) <= n; i++)
             {
                 if (n % i == 0)
                     return false;
             }
 
-            primeCache.Add(n);
             return true;
         }
     }
