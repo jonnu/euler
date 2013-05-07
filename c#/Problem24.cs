68a84b69e72f0f8c94fc07a585872ac39b29cdd7
jonnu <jontce@gmail.com>
2013-05-07 17:09:32 +0300

    Adding shells of skipped problems

---
+++ c#/Problem24.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler
+{
+    class Problem24 : Problem
+    {
+        public override void Process()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ c#/Problem26.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler
+{
+    class Problem26 : Problem
+    {
+        public override void Process()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
