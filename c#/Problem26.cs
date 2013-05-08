244be9e4087bf7746592aea99f936b35e7944126
jonnu <jontce@gmail.com>
2013-05-08 18:03:42 +0300

    Solution to problem 26

--- c#/Problem26.cs
+++ c#/Problem26.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Euler
 {
@@ -8,7 +6,39 @@ namespace Euler
     {
         public override void Process()
         {
-            Console.WriteLine("");
+            int limit = 1000;
+            int longest = FindLongestReciprocalCycle(limit);
+
+            Console.WriteLine("Integer with longest reciprocal cycle <= {0} = {1}", limit, longest);
+        }
+
+        private int FindLongestReciprocalCycle(int limit)
+        {
+            int longest = 0, sampled = limit;
+            for (int i = limit; i > 1; i--)
+            {
+                if (i - 1 < longest)
+                    break;
+
+                int j = 1, place = 0;
+                int[] remainders = new int[i];
+
+                while (remainders[j] == 0)
+                {
+                    remainders[j] = place;
+                    j = (j * 10) % i;
+                    place++;
+                }
+
+                // Did this result in a new maximum?
+                if (longest < (place - remainders[j]))
+                {
+                    longest = place - remainders[j];
+                    sampled = i;
+                }
+            }
+
+            return sampled;
         }
     }
 }
