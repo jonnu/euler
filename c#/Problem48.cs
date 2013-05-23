6b8cbfb017cf5a22a23158b04b5cbf19c040abd9
jonnu <jontce@gmail.com>
2013-05-23 17:26:49 +0300

    Solution to problem 48.
    BigInteger + LINQ again - kind of cheating.

---
+++ c#/Problem48.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+
+namespace Euler
+{
+    class Problem48 : Problem
+    {
+        public override void Process()
+        {
+            string disgustinglyLargeNumber = Enumerable.Range(1, 1000)
+                .Select(x => BigInteger.Pow((BigInteger)x, x))
+                .Aggregate((x, y) => BigInteger.Add(x, y))
+                .ToString();
+
+            Console.WriteLine(disgustinglyLargeNumber.Substring(disgustinglyLargeNumber.Length - 10));
+        }
+    }
+}
