5edbad5a584d4afbeddae5ed441320176f8cb8b8
jonnu <jontce@gmail.com>
2013-05-24 14:54:17 +0300

    Solution to problem 49 & generic Prime class.

---
+++ c#/Problem49.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler
+{
+    class Problem49 : Problem
+    {
+        public override void Process()
+        {
+            int upperLimit = 10000;
+
+            PrimeGenerator generator = new PrimeGenerator();
+            List<int> primes = generator.Between(1000, upperLimit);
+
+            for (int i = 0; i < primes.Count(); i++)
+            {
+                int a = primes[i];
+                for (int j = i + 1; j < primes.Count(); j++)
+                {
+                    int b = primes[j];
+                    int c = b + (b - a);
+                    if (c > upperLimit)
+                        break;
+
+                    if (generator.IsPrime(c) && ArePermutable(a, b, c))
+                    {
+                        Console.WriteLine("{0}{1}{2} (Step: {3})", a, b, c, c - b);
+                    }
+                }
+            }
+        }
+
+        private bool ArePermutable(params int[] values)
+        {
+            int[] digits = new int[10];
+
+            for (int i = 0; i < values.Count(); i++)
+            {
+                int[] check = new int[10];
+                for (int j = (int)Math.Log10(values[i]) + 1; j > 0; j--)
+                {
+                    int digit = values[i] % 10;
+                    values[i] /= 10;
+
+                    if (i == 0)
+                    {
+                        digits[digit]++;  
+                    }
+                    else
+                    {
+                        check[digit]++;
+                    }
+                }
+
+                if (i == 0)
+                    continue;
+
+                for (int d = 0; d < 10; d++)
+                {
+                    if (digits[d] - check[d] != 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ c#/common/PrimeGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler
+{
+    class PrimeGenerator
+    {
+        private BitArray primeCache;
+        private List<int> primes;
+
+        public PrimeGenerator() { }
+
+        public bool IsPrime(int value)
+        {
+            return primeCache[value];
+        }
+
+        public List<int> Below(int limit)
+        {
+            return SieveOfEratosthenes(limit);
+        }
+
+        public List<int> Between(int lower, int upper)
+        {
+            return SieveOfEratosthenes(upper, x => lower <= x);
+        }
+
+        private List<int> SieveOfEratosthenes(int limit, Func<int, bool> filterFunction = null)
+        {
+            primeCache = new BitArray(limit + 1, true);
+            for (int i = 2; i < Math.Sqrt(limit); i++)
+            {
+                if (primeCache[i])
+                {
+                    for (int j = i * 2; j < limit; j += i)
+                    {
+                        primeCache[j] = false;
+                    }
+                }
+            }
+
+            primes = new List<int>();
+            for (int a = 2; a < limit; a++)
+            {
+                if (primeCache[a] && (filterFunction != null && filterFunction(a)))
+                {
+                    primes.Add(a);
+                }
+            }
+
+            return primes;
+        }
+    }
+}
