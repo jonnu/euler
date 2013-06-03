270cac69631397abb00f46b8391ae68c58cdd78b
jonnu <jontce@gmail.com>
2013-06-03 16:28:47 +0300

    Solution to problem 55

---
+++ c#/Problem55.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler
+{
+    class Problem55 : Problem
+    {
+        public override void Process()
+        {
+            int lychrelLimit = 10000;
+            int lychrelCount = Enumerable
+                .Range(1, lychrelLimit)
+                .Where(x => IsLychrelNumber(new LargeInt(x), 50))
+                .Count();
+
+            Console.WriteLine("{0} Lychrel numbers below {1}", lychrelCount, lychrelLimit);
+        }
+
+        private bool IsLychrelNumber(LargeInt value, int loopLimit)
+        {
+            while (loopLimit > 0)
+            {
+                value += new LargeInt(value).Reverse();
+                if (value.Palindrome())
+                    return false;
+
+                loopLimit--;
+            }
+
+            return true;
+        }
+    }
+}
